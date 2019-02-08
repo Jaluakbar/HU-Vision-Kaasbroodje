4e930e032cfe89ffe7f9bdea48833992740ebc10
thoom76 <thomas.smeele@student.hu.nl>
2019-02-08 13:42:44 +0100

    a start
    * custom vertex
    * convolution function
    * kind of edge detection

--- source/ExternalDLL/ExternalDLL/Main.cpp
+++ source/ExternalDLL/ExternalDLL/Main.cpp
@@ -19,14 +19,14 @@ int main(int argc, char * argv[]) {
 	//ImageFactory::setImplementation(ImageFactory::STUDENT);
 
 
-	ImageIO::debugFolder = "D:\\Users\\Rolf\\Downloads\\FaceMinMin";
+	ImageIO::debugFolder = "C:/ti-software/HU-Vision-Kaasbroodje/source/ExternalDLL/Debug";
 	ImageIO::isInDebugMode = true; //If set to false the ImageIO class will skip any image save function calls
 
 
 
 
 	RGBImage * input = ImageFactory::newRGBImage();
-	if (!ImageIO::loadImage("D:\\Users\\Rolf\\Downloads\\TestA5.jpg", *input)) {
+	if (!ImageIO::loadImage("C:/ti-software/HU-Vision-Kaasbroodje/source/ExternalDLL/Debug/child1.png", *input)) {
 		std::cout << "Image could not be loaded!" << std::endl;
 		system("pause");
 		return 0;
@@ -74,7 +74,7 @@ bool executeSteps(DLLExecution * executor) {
 	}
 	ImageIO::saveIntensityImage(*executor->resultPreProcessingStep2, ImageIO::getDebugFileName("Pre-processing-2.png"));
 
-	if (!executor->executePreProcessingStep3(false)) {
+	if (!executor->executePreProcessingStep3(true)) {
 		std::cout << "Pre-processing step 3 failed!" << std::endl;
 		return false;
 	}
--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,5 +1,6 @@
 #include "StudentPreProcessing.h"
-
+#include "edge_detection.h"
+#include <iostream>
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
 	return nullptr;
@@ -10,7 +11,48 @@ IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &imag
 }
 
 IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &image) const { // aanpassen
-	return nullptr;
+	ed::matrix<250,250> m(image);
+	ed::matrix<9, 9> kernel({ {
+	{0,0,0,1,1,1,0,0,0},
+	{0,0,0,1,1,1,0,0,0},
+	{0,0,0,1,1,1,0,0,0},
+	{1,1,1,-4,-4,-4,1,1,1},
+	{1,1,1,-4,-4,-4,1,1,1},
+	{1,1,1,-4,-4,-4,1,1,1},
+	{0,0,0,1,1,1,0,0,0},
+	{0,0,0,1,1,1,0,0,0},
+	{0,0,0,1,1,1,0,0,0}
+} });
+
+	auto convulutioned = ed::convolution(m, kernel);
+
+	for (unsigned int y = 0; y < image.getHeight(); y++) {
+		for (unsigned int x = 0; x < image.getWidth(); x++) {
+
+			auto c = m[y][x];
+			uint8_t number = 0;
+			for (unsigned int i = 0; i < 8; i++) {
+				((c >> i) & 0b1) ? number += pow(2, i) : number += 0;
+			}
+			std::cout << ((int)number < 220) ? 1 : 0;
+		}
+		std::cout << '\n';
+	}
+
+	for (unsigned int y = 0; y < image.getHeight(); y++) {
+		for (unsigned int x = 0; x < image.getWidth(); x++) {
+
+			auto c = convulutioned[y][x];
+			uint8_t number = 0;
+			for (unsigned int i = 0; i < 8; i++) {
+				((c >> i) & 0b1) ? number += pow(2, i) : number += 0;
+			}
+			std::cout << ((int)number < 220)?1:0 ;
+		}
+		std::cout << '\n';
+	}
+	//return nullptr;
+	return convulutioned.get_intensity_image_ptr();
 }
 
 IntensityImage * StudentPreProcessing::stepThresholding(const IntensityImage &image) const { // aanpassen
